f6795916c0b98481fff823754af2a1c2e35dab3c
Matías Antunez <antunezmatias@gmail.com>
2021-06-25 13:22:25 -0300

    Revert "tg 2351 - Modelos Cart y Product"
    This reverts commit 9c45fbc178a911125388c02ce4c736f349895731

--- shopping-cart-Nico/src/main/java/ShoppingCartAplication.java
+++
@@ -1,9 +0,0 @@
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-class ShoppingCartApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(ShoppingCartApplication.class, args);
-    }
-}

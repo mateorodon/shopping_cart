dbd6ba814d2ea34d8306b43b5fb7e9455d5ccf68
Mateo Rodon <mateorodon123@gmail.com>
2023-10-10 12:41:03 -0300

    tg-310 Implementa DTORequest de CrtProduct

---
+++ back-end/cart/src/main/java/ar/lamansys/cart/ha/infrastructure/in/rest/cart_product/CartProductDTORequest.java
@@ -0,0 +1,16 @@
+package ar.lamansys.cart.ha.infrastructure.in.rest.cart_product;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CartProductDTORequest {
+
+    private Integer product_id;
+    private Integer amount;
+}

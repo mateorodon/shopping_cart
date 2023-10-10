7ad4e4f72aeb4801e1b5bc9b54efa801265076d5
Mateo Rodon <mateorodon123@gmail.com>
2023-10-10 12:24:49 -0300

    tg-303 Crea CartPort

---
+++ back-end/cart/src/main/java/ar/lamansys/cart/hb/application/cart/port/CartPort.java
@@ -0,0 +1,17 @@
+package ar.lamansys.cart.hb.application.cart.port;
+
+import ar.lamansys.cart.hc.domain.cart.CartBO;
+
+import java.util.Optional;
+
+public interface CartPort {
+
+    CartBO save(CartBO cart);
+
+    boolean exists(Integer cart_id,Integer user_id);
+
+    Optional<CartBO> findById(Integer cart_id,Integer user_id);
+
+    CartBO findByUser_id(Integer user_id);
+
+}

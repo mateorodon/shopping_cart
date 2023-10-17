8624f99ace800362b07302509a49dc25152c9e27
Mateo Rodon <mateorodon123@gmail.com>
2023-10-17 13:58:41 -0300

    tg-572 Crea script para creación de proyecto básica en la DB

---
+++ back-end/app/src/main/resources/db/changelog/db.changelog-inic_cart.sql
@@ -0,0 +1,9 @@
+INSERT INTO users(username) VALUES ('user1');
+INSERT INTO users(username) VALUES ('user2');
+INSERT INTO users(username) VALUES ('user3');
+
+INSERT INTO product (name, stock, price) VALUES ('arroz', 120, 275);
+INSERT INTO product (name, stock, price) VALUES ('lentejas', 50, 320);
+INSERT INTO product (name, stock, price) VALUES ('pan', 200, 600);
+INSERT INTO product (name, stock, price) VALUES ('fideos', 100, 450.5);
+INSERT INTO product (name, stock, price) VALUES ('tomate', 150, 42.5);
